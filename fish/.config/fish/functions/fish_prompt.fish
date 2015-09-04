65883dbbad72ecb4d9a13121aa147f8cd3c97790
enricobacis <enrico.bacis@gmail.com>
2015-09-04 16:32:28 +0200

    fish: yellow prompt if in ssh, otherwise green

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    set_color -o green
+    if test "$SSH_CLIENT$SSH_TTY"; set_color -o yellow; else; set_color -o green; end
     echo -ns (whoami) '@' (hostname | cut -d. -f1) ' '
     set_color -o blue
     echo -ns (prompt_pwd) '> '

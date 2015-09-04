21e26ede26a2ab45a8f92b3688bfb54734fbaa60
enricobacis <enrico.bacis@gmail.com>
2015-09-04 11:01:00 +0200

    fish: add colored prompt

---
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,7 @@
+function fish_prompt
+    set_color -o green
+    echo -ns (whoami) '@' (hostname | cut -d. -f1) ' '
+    set_color -o blue
+    echo -ns (prompt_pwd) ' > '
+    set color normal
+end

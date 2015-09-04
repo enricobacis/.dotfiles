b4b932cb338449cbf46555e4902d34adc62a7b65
enricobacis <enrico.bacis@gmail.com>
2015-09-04 14:47:28 +0200

    fish: i like the ~>

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,6 +2,6 @@ function fish_prompt
     set_color -o green
     echo -ns (whoami) '@' (hostname | cut -d. -f1) ' '
     set_color -o blue
-    echo -ns (prompt_pwd) ' > '
+    echo -ns (prompt_pwd) '> '
     set color normal
 end

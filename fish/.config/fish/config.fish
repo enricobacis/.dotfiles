68b8185e6fd5a7fd61caec71ba88edb1b348f2af
enricobacis <enrico.bacis@gmail.com>
2015-09-09 12:37:11 +0200

    fish: use full path for vim and vim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,5 @@
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
 # editors
-set EDITOR vim
-set VISUAL vim
+set EDITOR /usr/bin/vim
+set VISUAL /usr/bin/vim

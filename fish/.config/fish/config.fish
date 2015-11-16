6afa4e2c2665c27a953c941b69148df071668563
enricobacis <enrico.bacis@gmail.com>
2015-11-16 14:56:10 +0100

    fish: make EDITOR and VISUAL globally exported variables.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,8 +2,8 @@
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
 # editors
-set EDITOR /usr/bin/vim
-set VISUAL /usr/bin/vim
+set -gx EDITOR /usr/bin/vim
+set -gx VISUAL /usr/bin/vim
 
 #vagrant
 set -x VAGRANT_DEFAULT_PROVIDER virtualbox

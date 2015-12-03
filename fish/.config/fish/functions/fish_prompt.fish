fb0f8817e8139da324aaa20f8c9a7689ac0d6d9b
enricobacis <enrico.bacis@gmail.com>
2015-12-03 11:18:07 +0100

    fish: separate echo flags for compatibility

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
     if test "$SSH_CLIENT$SSH_TTY"; set_color -o yellow; else; set_color -o green; end
-    echo -ns (whoami) '@' (hostname | cut -d. -f1) ' '
+    echo -n -s (whoami) '@' (hostname | cut -d. -f1) ' '
     set_color -o blue
-    echo -ns (prompt_pwd) '> '
+    echo -n -s (prompt_pwd) '> '
     set color normal
 end

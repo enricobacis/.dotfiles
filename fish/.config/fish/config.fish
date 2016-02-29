32a628ddde9294e5b54603445ba5d17ae6454bc2
enricobacis <enrico.bacis@gmail.com>
2016-02-29 21:59:46 +0100

    fish: add LC_ALL and LANG

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,8 +8,12 @@ set -gx VISUAL /usr/bin/vim
 # terminal
 set -gx TERMINAL sakura
 
-#vagrant
+# vagrant
 set -x VAGRANT_DEFAULT_PROVIDER virtualbox
+
+# languages
+set -gx LC_ALL "en_US.UTF-8"
+set -gx LANG "en_US.UTF-8"
 
 # start X at login
 if status --is-login

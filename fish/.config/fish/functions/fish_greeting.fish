1378559b7218974bf493ce27d9ecae9aede490f3
enricobacis <enrico.bacis@gmail.com>
2016-03-18 12:47:51 +0100

    fish: automatically download oh-my-fish

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,3 +1,10 @@
 function fish_greeting
-	
+    if not test -d ~/.local/share/omf
+        install_omf
+    end
+end
+
+function install_omf
+    echo "[Downloading Oh-My-Fish ...]"
+    curl -# -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 end

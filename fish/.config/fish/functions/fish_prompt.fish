2bb57e525ca5a205531f9c3df77d0b5fb7679064
enricobacis <enrico.bacis@gmail.com>
2016-03-18 13:50:07 +0100

    fish: remove fish prompt and add oh-my-fish folder

--- fish/.config/fish/functions/fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-    if test "$SSH_CLIENT$SSH_TTY"; set_color -o yellow; else; set_color -o green; end
-    echo -n -s (whoami) '@' (hostname | cut -d. -f1) ' '
-    set_color -o blue
-    echo -n -s (prompt_pwd) '> '
-    set color normal
-end

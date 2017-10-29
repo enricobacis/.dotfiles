60d62be885532ce74bc20a575c8c1b3857e2b3a4
enricobacis <enrico.bacis@gmail.com>
2017-10-29 22:42:16 +0100

    vim: add default for ycm_extra_conf for C/C++

---
+++ vim/.vim/utils/ycm_extra_conf.py
@@ -0,0 +1,29 @@
+def FlagsForFile(filename, **kwargs):
+
+    flags = [
+      '-Wall',
+      '-Wextra',
+      '-Werror'
+      '-pedantic',
+      '-I',
+      '.',
+      '-isystem',
+      '/usr/include',
+      '-isystem',
+      '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+      '-isystem',
+      '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/clang/5.1/include',
+      '-isystem',
+      '/usr/local/include',
+      '-isystem',
+      '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
+    ]
+
+    filetype = kwargs['client_data']['&filetype']
+
+    if filetype == 'c': flags += ['-xc']
+    elif filetype == 'cpp': flags += ['-xc++', '-std=c++11']
+    elif filetype == 'objc': flags += ['-ObjC']
+    else: flags = []
+
+    return { 'flags': flags, 'do_cache': True }
--- vim/.vimrc
+++ vim/.vimrc
@@ -175,6 +175,8 @@ let g:ycm_confirm_extra_conf = 0
 let g:ycm_add_preview_to_completeopt = 1
 let g:ycm_autoclose_preview_window_after_completion = 1
 let g:ycm_goto_buffer_command = 'new-or-existing-tab'
+let g:ycm_extra_conf_vim_data = ['&filetype']
+let g:ycm_global_ycm_extra_conf = '~/.vim/utils/ycm_extra_conf.py'
 nnoremap <leader>f :YcmCompleter FixIt<CR>
 nnoremap <leader>g :YcmCompleter GoTo<CR>
 

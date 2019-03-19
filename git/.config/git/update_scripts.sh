c58fa4fd3a162b0236b4112b4e6322f9fd8aeb59
enricobacis <enrico.bacis@gmail.com>
2019-03-19 14:31:57 +0100

    git: add git-latexdiff to scripts

--- git/.config/git/update_scripts.sh
+++ git/.config/git/update_scripts.sh
@@ -6,3 +6,7 @@ echo "BASEDIR = ${BASEDIR}"
 echo '[Updating diff-so-fancy]'
 wget -q 'https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy' -O "${BASEDIR}/diff-so-fancy"
 chmod a+x "${BASEDIR}/diff-so-fancy"
+
+echo '[Updating git-latexdiff]'
+wget -q 'https://gitlab.com/git-latexdiff/git-latexdiff/raw/master/git-latexdiff' -O "${BASEDIR}/git-latexdiff"
+chmod a+x "${BASEDIR}/git-latexdiff"

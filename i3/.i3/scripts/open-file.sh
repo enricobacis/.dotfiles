3ecbc8c34943d11df3e1f20e302bbce36bbff666
enricobacis <enrico.bacis@gmail.com>
2019-04-16 15:26:51 +0200

    i3: fuzzy find files and open

---
+++ i3/.i3/scripts/open-file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z ${___inner+x} ]
+then
+    export ___inner=1
+    $TERMINAL -x "bash -c $0"
+else
+    file=$(fzf)
+    nohup xdg-open $file >/dev/null 2>&1 &
+    sleep 0.1
+fi

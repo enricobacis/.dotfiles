d4c2cc73fbdabe52317655f63af23040e8ea1b18
enricobacis <enrico.bacis@gmail.com>
2019-05-31 13:29:28 +0200

    fish: improve clean-pdf

--- fish/.config/fish/functions/clean-pdf.fish
+++ fish/.config/fish/functions/clean-pdf.fish
@@ -1,23 +1,35 @@
-function clean-pdf --argument filename --description "Convert text pdf into images pdf and strip metadata"
+function clean-pdf --argument filename --description "Converts text pdfs into images pdfs and strips metadata"
 
-	if test -z $filename
-		echo "Usage: clean-pdf filename.pdf"
+	if test -z $filename; or not test -e $filename
+		echo "Usage: `clean-pdf filename.pdf` or `clean-pdf dirname`"
 		return 1
 	end
 
-	set -l cleanname "clean/$filename"
-	mkdir -p clean; and \
-	convert -density 200 "$filename" "$cleanname"; and \
-	exiftool -ignoreMinorErrors -all:all= "$cleanname"; and \
-	exiftool -delete_original! "$cleanname"; and \
-	qpdf --linearize "$cleanname" "$cleanname.2"; and \
-	mv "$cleanname.2" "$cleanname"
+	if test -d $filename
+		for f in $filename/*.pdf
+			echo "Cleaning $f ..."
+			clean-pdf $f
+		end
 
-	if test $status -eq 0
-		echo -e "\n[$cleanname created]"
-		return 0
+	else if test -f $filename
+		set -l cleanname "clean/$filename"
+		mkdir -p clean; and \
+		convert -density 200 "$filename" "$cleanname"; and \
+		exiftool -ignoreMinorErrors -all:all= "$cleanname"; and \
+		exiftool -delete_original! "$cleanname"; and \
+		qpdf --linearize "$cleanname" "$cleanname.2"; and \
+		mv "$cleanname.2" "$cleanname"
+
+		if test $status -eq 0
+			echo -e "\n[$cleanname created]"
+			return 0
+		else
+			echo -e "\n[Error creating file $cleanname]"
+			return 1
+		end
+
 	else
-		echo -e "\n[Error creating file $cleanname]"
+		echo "Error: Unrecognized file $filename"
 		return 1
 	end
 

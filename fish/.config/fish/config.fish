bfe745c0dc61939601007450e3958a66efa93b36
enricobacis <enrico.bacis@gmail.com>
2020-05-12 21:26:06 +0200

    more proper fisher installation

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,7 +32,11 @@ if begin status --is-login; and uname | grep -qi 'linux'; end
 end
 
 # fisherman
-if not functions -q fisher; eval (curl -sfL https://git.io/fisher-install); end
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
 
 # fisherman/done
 set -g __done_min_cmd_duration 10000

869558819725ae89a35b98889a5212e6a6b71f3e
enricobacis <enrico.bacis@gmail.com>
2020-05-26 12:02:57 +0200

    improve quality for scan function

--- fish/.config/fish/functions/scan.fish
+++ fish/.config/fish/functions/scan.fish
@@ -1,14 +1,15 @@
+# Defined in /var/folders/13/fc5j_gys3t16cfgxvcb6187r0000gn/T//fish.Z8orH2/scan.fish @ line 2
 function scan --description 'Make a pdf look like scanned' --argument filename
   if test -z $filename; or not test -f $filename
     echo "Usage: `scan filename.pdf`"
     return 1
   end
 
-  convert -density 150 "$argv[1]" \
+  convert -density 300 "$argv[1]" \
     -alpha Off \
-    -density 150 \
+    -density 300 \
     -colorspace Gray \
-    -blur 0.5x0.5 \
+    -blur 0.3x0.3 \
     -rotate "0.0"(random 5 9) \
     -attenuate 0.1 +noise Multiplicative \
     -quality 60 -compress jpeg \
